18f46b3f8c504501199c0051dce75b34f79e0271
fakhtehpanah <fakhipanah@gmail.com>
2025-02-21 14:47:08 +0330

    added .gitignore

---
+++ pages/index.js
@@ -0,0 +1,185 @@
+const menuButton = document.getElementById("menuButton");
+const dropdownMenu = document.getElementById("dropdownMenu");
+const btn = document.getElementById("btn");
+const listButton = document.getElementById("listButton");
+const buttons = document.querySelectorAll('button');
+const contents = document.querySelectorAll('.content');
+
+// tabs left part
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".tab-button");
+    const contents = document.querySelectorAll(".tab-content");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            buttons.forEach((btn) => btn.classList.remove("border-green-700"));
+            this.classList.add("focus:border-green-700");
+
+            contents.forEach((content) => content.classList.add("hidden"));
+
+            const tabId = this.getAttribute("data-tab");
+            document.getElementById(tabId).classList.remove("hidden");
+        });
+    });
+});
+
+
+menuButton.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("hidden");
+
+    if (dropdownMenu.classList.contains("hidden")) {
+        menuButton.innerHTML = "☰";
+    } else {
+        menuButton.innerHTML = "❌";
+    }
+});
+window.addEventListener('click', (e) => {
+    if (!menuButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
+      dropdownMenu.classList.add('hidden');
+    }
+  });
+
+  btn.addEventListener("click", () => {
+    listButton.classList.toggle("hidden");
+  });
+  window.addEventListener('click', (e) => {
+    if (!btn.contains(e.target) && !listButton.contains(e.target)) {
+      listButton.classList.add('hidden');
+    }
+  });
+
+
+// footer click vector for responsive
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleMenus = [
+        { buttonId: "arrow", menuId: "menu1" },
+        { buttonId: "arrow1", menuId: "menu2" },
+        { buttonId: "arrow2", menuId: "menu3" },
+    ];
+
+    toggleMenus.forEach(({ buttonId, menuId }) => {
+        const button = document.getElementById(buttonId);
+        const menu = document.getElementById(menuId);
+
+        button.addEventListener("click", function () {
+            if (window.innerWidth < 1024) {
+                menu.classList.toggle("hidden");
+                const isRotated = button.style.transform === "rotate(180deg)";
+                button.style.transform = isRotated ? "rotate(0deg)" : "rotate(180deg)";
+                button.style.transition = "transform 0.3s ease-in-out";
+            }
+        });
+    });
+});
+
+
+new Swiper('.myswiper',{
+    slidesPerView: 1,
+    loop: true,
+    speed: 400,
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    },
+    autoplay: {
+        delay: 2000,
+    },
+    spaceBetween: 10,
+    breakpoints: {
+        320: {
+            slidesPerView: 2
+        },
+        480: {
+            slidesPerView: 3
+        },
+        640: {
+            slidesPerView: 4
+        },
+        1024: {
+            slidesPerView: 5
+        }
+    }})
+
+new Swiper('.myswiper1',{
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    slidesPerView: 1,
+    loop: true,
+    speed: 400,
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    },
+    spaceBetween: 10,
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        480: {
+            slidesPerView: 2
+        },
+        640: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }})
+
+// blue section
+    function setupSearchableSelect(selectContainer) {
+        const selectBtn = selectContainer.querySelector("[data-select-btn]");
+        const dropdown = selectContainer.querySelector("[data-dropdown]");
+        const selectedOption = selectContainer.querySelector("[data-selected-option]");
+        const searchInput = selectContainer.querySelector("[data-search-input]");
+        const optionsList = selectContainer.querySelector("[data-options-list]").getElementsByTagName("li");
+    
+        selectBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            dropdown.classList.toggle("hidden");
+            searchInput.value = "";
+            filterOptions("", optionsList);
+            searchInput.focus();
+        });
+    
+        Array.from(optionsList).forEach(option => {
+            option.addEventListener("click", () => {
+                selectedOption.textContent = option.textContent;
+                dropdown.classList.add("hidden");
+            });
+        });
+    
+        searchInput.addEventListener("input", (e) => {
+            filterOptions(e.target.value, optionsList);
+        });
+    
+        function filterOptions(searchText, options) {
+            Array.from(options).forEach(option => {
+                if (option.textContent.includes(searchText)) {
+                    option.classList.remove("hidden");
+                } else {
+                    option.classList.add("hidden");
+                }
+            });
+        }
+    
+        document.addEventListener("click", (e) => {
+            if (!selectBtn.contains(e.target) && !dropdown.contains(e.target)) {
+                dropdown.classList.add("hidden");
+            }
+        });
+    }
+    
+    document.querySelectorAll("[data-select-container]").forEach(setupSearchableSelect);
+
+
+// لست مشاغل(مواردبیشتر)
+document.getElementById("toggleButton").addEventListener("click", function () {
+    document.querySelectorAll(".more-items").forEach(item => {
+        item.classList.toggle("hidden");
+    });
+
+    this.textContent = this.textContent === "موارد بیشتر" ? "موارد کمتر" : "موارد بیشتر";
+});
---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode : "class",
+  content: ["./pages/*.html"],
+  theme: {
+    extend: {
+
+      fontFamily: {
+        iransans: ["IranSans", "sans-serif"], // اضافه کردن فونت به Tailwind
+      },
+
+      container: {
+        center: true,
+        padding: '1.5rem',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1632px'
+        }
+      },
+      colors: {
+        white: '#FFFFFF',
+        gray: {
+          100: '#444',
+          200: '#555',
+          50: '#a5a5a5',
+          300: '#999',
+          400: '#777',
+        },
+        yellow: {
+          100: '#fdcb6e',
+          200: '#FDC040'
+        },
+        blue: {
+          100: '#3ab1e4',
+          200: '#008891',
+          300: '#253D4E'
+        },
+        green: {
+          100: '#F2FCE4',
+          150: '#BCE3C9',
+          200: '#3BB77E',
+          300: '#81B13D',
+          400: '#3A882B',
+          500: '#227226',
+          600: '#1A5D2C',
+          700: '#1abc9c'
+        },
+        brown: {
+          500: '#F0E8D5'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
