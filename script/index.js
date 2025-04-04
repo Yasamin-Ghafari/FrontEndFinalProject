f9a04a3505f6c4d4de249034c68ed6f30b963b3f
Yasamin - Ghafari <yasimahghafari@gmail.com>
2025-04-04 01:49:10 -0700

    edit slider section in home page & edit some detail

--- script/index.js
+++ script/index.js
@@ -1,3 +1,34 @@
+// 50 شغل برتر
+
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionHeader = document.getElementById("accordion-header");
+    const accordionContent = document.getElementById("accordion-content");
+    const accordionIcon = document.getElementById("accordion-icon");
+    const imageContainer = document.getElementById("image-container");
+    const imagePlaceholder = document.getElementById("image-placeholder");
+
+    let isOpen = false;
+
+    accordionHeader.addEventListener("click", function () {
+        isOpen = !isOpen;
+
+        if (isOpen) {
+            accordionContent.style.maxHeight = "500px";
+            accordionContent.style.opacity = "1";
+
+            // Move image to the second div
+            imagePlaceholder.appendChild(imageContainer.firstElementChild);
+        } else {
+            accordionContent.style.maxHeight = "0";
+            accordionContent.style.opacity = "0";
+
+            // Move image back to the first div
+            imageContainer.appendChild(imagePlaceholder.firstElementChild);
+        }
+    });
+});
+
+
 new Swiper('.myswiper',{
     slidesPerView: 1,
     loop: true,
@@ -105,42 +136,42 @@ document.addEventListener("DOMContentLoaded", function () {
 // });
 
 
-const loginTab = document.getElementById('loginTab');
-const registerTab = document.getElementById('registerTab');
-const loginContent = document.getElementById('loginContent');
-const registerContent = document.getElementById('registerContent');
-
-loginTab.addEventListener('click', () => {
-    loginTab.classList.add('border-blue-500', 'text-blue-500');
-    registerTab.classList.remove('border-blue-500', 'text-blue-500');
-    loginContent.classList.remove('hidden');
-    registerContent.classList.add('hidden');
-});
-
-registerTab.addEventListener('click', () => {
-    registerTab.classList.add('border-blue-500', 'text-blue-500');
-    loginTab.classList.remove('border-blue-500', 'text-blue-500');
-    registerContent.classList.remove('hidden');
-    loginContent.classList.add('hidden');
-});
-
-// پیش‌فرض نمایش تب ورود
-loginTab.classList.add('border-blue-500', 'text-blue-500');
-loginContent.classList.remove('hidden');
+// const loginTab = document.getElementById('loginTab');
+// const registerTab = document.getElementById('registerTab');
+// const loginContent = document.getElementById('loginContent');
+// const registerContent = document.getElementById('registerContent');
+//
+// loginTab.addEventListener('click', () => {
+//     loginTab.classList.add('border-blue-500', 'text-blue-500');
+//     registerTab.classList.remove('border-blue-500', 'text-blue-500');
+//     loginContent.classList.remove('hidden');
+//     registerContent.classList.add('hidden');
+// });
+//
+// registerTab.addEventListener('click', () => {
+//     registerTab.classList.add('border-blue-500', 'text-blue-500');
+//     loginTab.classList.remove('border-blue-500', 'text-blue-500');
+//     registerContent.classList.remove('hidden');
+//     loginContent.classList.add('hidden');
+// });
+//
+// // پیش‌فرض نمایش تب ورود
+// loginTab.classList.add('border-blue-500', 'text-blue-500');
+// loginContent.classList.remove('hidden');
 
 
 
 //menu dropdown
-
-menuButton.addEventListener("click", () => {
-    dropdownMenu.classList.toggle("hidden");
-
-    if (dropdownMenu.classList.contains("hidden")) {
-        menuButton.innerHTML = "☰";
-    } else {
-        menuButton.innerHTML = "❌";
-    }
-});
+//
+// menuButton.addEventListener("click", () => {
+//     dropdownMenu.classList.toggle("hidden");
+//
+//     if (dropdownMenu.classList.contains("hidden")) {
+//         menuButton.innerHTML = "☰";
+//     } else {
+//         menuButton.innerHTML = "❌";
+//     }
+// });
 // window.addEventListener('click', (e) => {
 //     if (!menuButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
 //       dropdownMenu.classList.add('hidden');
@@ -185,35 +216,6 @@ menuButton.addEventListener("click", () => {
 
 
 
-// 50 شغل برتر 
-
-document.addEventListener("DOMContentLoaded", function () {
-    const accordionHeader = document.getElementById("accordion-header");
-    const accordionContent = document.getElementById("accordion-content");
-    const accordionIcon = document.getElementById("accordion-icon");
-    const imageContainer = document.getElementById("image-container");
-    const imagePlaceholder = document.getElementById("image-placeholder");
-
-    let isOpen = false;
-
-    accordionHeader.addEventListener("click", function () {
-        isOpen = !isOpen;
-
-        if (isOpen) {
-            accordionContent.style.maxHeight = "500px";
-            accordionContent.style.opacity = "1";
-
-            // Move image to the second div
-            imagePlaceholder.appendChild(imageContainer.firstElementChild);
-        } else {
-            accordionContent.style.maxHeight = "0";
-            accordionContent.style.opacity = "0";
-
-            // Move image back to the first div
-            imageContainer.appendChild(imagePlaceholder.firstElementChild);
-        }
-    });
-});
 
 
 
@@ -267,35 +269,35 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll("[data-select-container]").forEach(setupSearchableSelect);
 
 
-// لست مشاغل(مواردبیشتر)
-document.addEventListener("DOMContentLoaded", function () {
-    // گرفتن تمام دکمه‌های "موارد بیشتر" برای هر ستون
-    document.querySelectorAll("[id^=toggleButton]").forEach(button => {
-        button.addEventListener("click", function () {
-            // پیدا کردن ستون مربوطه و نمایش آیتم‌های مخفی
-            let parentDiv = this.closest("div").parentElement;
-            parentDiv.querySelectorAll(".more-items").forEach(item => {
-                item.classList.toggle("hidden");
-            });
-
-            // تغییر متن دکمه
-            this.textContent = this.textContent === "موارد بیشتر" ? "موارد کمتر" : "موارد بیشتر";
-        });
-    });
-
-    // عملکرد دکمه کلی برای حالت دسکتاپ
-    document.getElementById("toggleButton").addEventListener("click", function () {
-        let hiddenItems = document.querySelectorAll(".more-items.hidden");
-        let visibleItems = document.querySelectorAll(".more-items:not(.hidden)");
-
-        if (hiddenItems.length > 0) {
-            hiddenItems.forEach(item => item.classList.remove("hidden"));
-            this.textContent = "موارد کمتر";
-        } else {
-            visibleItems.forEach(item => item.classList.add("hidden"));
-            this.textContent = "موارد بیشتر";
-        }
-    });
-});
+// // لست مشاغل(مواردبیشتر)
+// document.addEventListener("DOMContentLoaded", function () {
+//     // گرفتن تمام دکمه‌های "موارد بیشتر" برای هر ستون
+//     document.querySelectorAll("[id^=toggleButton]").forEach(button => {
+//         button.addEventListener("click", function () {
+//             // پیدا کردن ستون مربوطه و نمایش آیتم‌های مخفی
+//             let parentDiv = this.closest("div").parentElement;
+//             parentDiv.querySelectorAll(".more-items").forEach(item => {
+//                 item.classList.toggle("hidden");
+//             });
+//
+//             // تغییر متن دکمه
+//             this.textContent = this.textContent === "موارد بیشتر" ? "موارد کمتر" : "موارد بیشتر";
+//         });
+//     });
+//
+//     // عملکرد دکمه کلی برای حالت دسکتاپ
+//     document.getElementById("toggleButton").addEventListener("click", function () {
+//         let hiddenItems = document.querySelectorAll(".more-items.hidden");
+//         let visibleItems = document.querySelectorAll(".more-items:not(.hidden)");
+//
+//         if (hiddenItems.length > 0) {
+//             hiddenItems.forEach(item => item.classList.remove("hidden"));
+//             this.textContent = "موارد کمتر";
+//         } else {
+//             visibleItems.forEach(item => item.classList.add("hidden"));
+//             this.textContent = "موارد بیشتر";
+//         }
+//     });
+// });
 
 
